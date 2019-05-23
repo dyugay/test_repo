a96188b7fa010ac76d01b8e13731d71468f11f08
dyugay <dyugay@yandex.ru>
2019-05-23 20:14:14 +0600

    first commit in the dev branch

--- index.php
+++ index.php
@@ -7,7 +7,9 @@ echo 'Hello, world';
 //did the second change in the project
 echo 'pls, tell the story';
 
+//did the branch to fix issue 01
 
+$issue = 1;
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
