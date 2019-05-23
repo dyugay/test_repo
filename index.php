77a1832289dc96bdcd67cbdef7c2a062e33867c3
dyugay <dyugay@yandex.ru>
2019-05-23 20:07:14 +0600

    first commit in production branch

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+//first code line in this test project
+echo 'Hello, world';
+
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
